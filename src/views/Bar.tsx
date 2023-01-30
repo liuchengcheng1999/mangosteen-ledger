28eb50c0871b9658ccb8c5589e9e53a09d5b31f5
liuchengcheng1999 <liuchengcheng233@gmail.com>
2023-01-30 11:10:49 +0800

    修复Bar组件的bug

--- src/views/Bar.tsx
+++ src/views/Bar.tsx
@@ -2,7 +2,7 @@ import { defineComponent } from "vue";
 export const Bar = defineComponent({
   setup: (props, context) => {
     return () => (
-      <div></div>
+      <div>Bar</div>
     )
   }
 });
