1a9a89aa6e46703b3f00e4d2cdbfcfc696c7a9db
liuchengcheng1999 <liuchengcheng233@gmail.com>
2023-03-20 13:23:11 +0800

    完成 TagEdit 页面的样式

--- src/components/tag/TagCreate.tsx
+++ src/components/tag/TagCreate.tsx
@@ -1,16 +1,9 @@
-import { defineComponent, PropType, reactive } from 'vue';
+import { defineComponent, reactive } from 'vue';
 import { MainLayout } from '../../layouts/MainLayout';
-import { Button } from '../../shared/Button';
-import s from './TagCreate.module.scss';
 import { Icon } from '../../shared/Icon';
-import { EmojiSelect } from '../../shared/EmojiSelect';
 import {Rules, validate } from '../../shared/validate';
+import { TagForm } from './TagForm';
 export const TagCreate = defineComponent({
-  props: {
-    name: {
-      type: String as PropType<string>
-    }
-  },
   setup: (props, context) => {
     const formData = reactive({
       name: '',
@@ -36,37 +29,7 @@ export const TagCreate = defineComponent({
         title: () => '新建标签',
         icon: () => <Icon name="left" onClick={() => { }} />,
         default: () => (
-          <form class={s.form} onSubmit={onSubmit}>
-            <div class={s.formRow}>
-              <label class={s.fromLabel}>
-                <span class={s.formItem_name}>标签名</span>
-                <div class={s.formItem_value}>
-                  <input v-model={formData.name} class={[s.formItem, s.input, s.error]}></input>
-                </div>
-                <div class={s.formItem_errorHint}>
-                {/* 这里换成中文全角空格防止抖动 */}
-                  <span>{errors['name'] ? errors['name'][0] : '　'}</span>
-                </div>
-              </label>
-            </div>
-            <div class={s.formRow}>
-              <label class={s.formLabel}>
-                <span class={s.formItem_name}>符号 {formData.sign}</span>
-                <div class={s.formItem_value}>
-                  <EmojiSelect v-model={formData.sign} class={[s.formItem, s.emojiList, s.error]} />
-                </div>
-                <div class={s.formItem_errorHint}>
-                  <span>{errors['sign'] ? errors['sign'][0] : '　'}</span>
-                </div>
-              </label>
-            </div>
-            <p class={s.tips}>记账时长按标签即可进行编辑</p>
-            <div class={s.formRow}>
-              <div class={s.formItem_value}>
-                <Button class={[s.formItem, s.button]}>确定</Button>
-              </div>
-            </div>
-          </form>
+          <TagForm/>
         )
       }}
       </MainLayout>
--- src/components/tag/TagEdit.tsx
+++ src/components/tag/TagEdit.tsx
@@ -1,13 +1,44 @@
-import { defineComponent, PropType } from 'vue';
+import { defineComponent, reactive } from 'vue';
+import { MainLayout } from '../../layouts/MainLayout';
+import { Rules, validate } from '../../shared/validate';
+import { Icon } from '../../shared/Icon';
+import { TagForm } from './TagForm';
+import { Button } from '../../shared/Button';
+import s from './Tag.module.scss';
 export const TagEdit = defineComponent({
-    props: {
-    name: {
-        type: String as PropType<string>
-    }
-},
     setup: (props, context) => {
-        return() => (
-            <div>edit</div>
+        const formData = reactive({
+          name: '',
+          sign: '',
+        })
+        const errors = reactive<{[k in keyof typeof formData]?: string[]}>({})
+        const onSubmit = (e: Event) => {
+          const rules: Rules<typeof formData> = [
+            { key: 'name', type: 'required', message: '必填' },
+            { key: 'name', type: 'pattern', regex: /^.{1,4}$/, message: '只能填 1 到 4 个字符' },
+            { key: 'sign', type: 'required', message: '必填' },
+          ]
+          Object.assign(errors, {
+            name: undefined,
+            sign: undefined
+          })
+          Object.assign(errors, validate(formData, rules))
+          e.preventDefault()
+        }
+        return () => (
+          <MainLayout>
+            {{
+            title: () => '新建标签',
+            icon: () => <Icon name="left" onClick={() => { }} />,
+            default: () => <>
+                <TagForm />
+                <div class={s.actions}>
+                    <Button level='danger' class={s.removeTags} onClick={() => {}}>删除标签</Button>
+                    <Button level='danger' class={s.removeTagsAndItems} onClick={() => {}}>删除标签和记账</Button>
+                </div>
+            </>
+          }}
+          </MainLayout>
         )
-    }
+      }
 })
