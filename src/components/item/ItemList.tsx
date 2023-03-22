f0e93bbbef29db4e22169b9c29a27df00adb8cc3
liuchengcheng1999 <liuchengcheng233@gmail.com>
2023-03-22 12:15:06 +0800

    完成itemList

--- src/components/item/ItemList.tsx
+++ src/components/item/ItemList.tsx
@@ -1,5 +1,8 @@
-import { defineComponent, PropType } from 'vue';
+import { defineComponent, PropType, ref } from 'vue';
 import s from './ItemList.module.scss';
+import { MainLayout } from '../../layouts/MainLayout';
+import { Tab, Tabs } from '../../shared/Tabs';
+import { Icon } from '../../shared/Icon';
 export const ItemList = defineComponent({
     props: {
     name: {
@@ -7,8 +10,30 @@ export const ItemList = defineComponent({
     }
 },
     setup: (props, context) => {
+        const refSelected = ref('本月')
         return() => (
-            <div class={s.wrapper}>hi</div>
+           <MainLayout>
+            {{
+                title: () => '山竹记账',
+                icon: () => <Icon name="menu"/>,
+                default: () => (
+                    <Tabs classPrefix={'customTabs'} v-model:selected={refSelected.value}>
+                        <Tab name="本月">
+                            list1
+                        </Tab>
+                        <Tab name="上月">
+                            list2
+                        </Tab>
+                        <Tab name="今年">
+                            list3
+                        </Tab>
+                        <Tab name="自定义时间">
+                            list4
+                        </Tab>
+                    </Tabs>
+                )
+            }}
+           </MainLayout>
         )
     }
 })
